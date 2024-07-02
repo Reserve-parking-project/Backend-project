6613352c8b0fa0bb54eb665bf0cccfe0fdaaadc1
Mrexyha <104315683+Mrexyha@users.noreply.github.com>
2024-07-02 12:53:29 +0300

    Connect to local DB

--- server.js
+++ server.js
@@ -1,7 +1,21 @@
+const mongoose = require('mongoose');
+
 const dotenv = require('dotenv');
 const app = require('./app');
 
 dotenv.config({ path: './config.env' });
+
+// Local DB
+mongoose
+  .connect(process.env.DATABASE_LOCAL, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true,
+  })
+  .then((con) => {
+    console.log('DB connection successful');
+  });
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
