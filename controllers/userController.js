a47dd5121142af23e0a506a1d9e497eb05f3f939
Mrexyha <104315683+Mrexyha@users.noreply.github.com>
2024-08-17 20:28:46 +0300

    Update Current User Data

--- controllers/userController.js
+++ controllers/userController.js
@@ -25,6 +25,29 @@ exports.getAllUsers = catchAsync(async (req, res, next) => {
   });
 });
 
-exports.updateMe = catchAsync(async (req, res, next) => {});
+exports.updateMe = catchAsync(async (req, res, next) => {
+  if (req.body.password || req.body.passwordConfirm) {
+    return next(
+      new AppError(
+        'This route is not for password updates. Please use /updateMyPassword',
+        400
+      )
+    );
+  }
+
+  const filteredBody = filterObj(req.body, 'firstName', 'lastName', 'email');
+
+  const updatedUser = await User.findByIdAndUpdate(req.user.id, filteredBody, {
+    new: true,
+    runValidators: true,
+  });
+
+  res.status(200).json({
+    status: 'success',
+    data: {
+      user: updatedUser,
+    },
+  });
+});
 
 exports.deleteMe = catchAsync(async (req, res, next) => {});
